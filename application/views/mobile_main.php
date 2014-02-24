07cae5ad86c907566e945c7c246582371273dfcd
happyInCorner <xiaofosong@126.com>
2014-02-24 10:19:17 +0800

    start

---
+++ application/views/mobile_main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--HTML5 doctype-->
+<html>
+	<head>
+	    <title>我的读书小小站</title>
+	    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	     <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	</head> 
+<body>      
+ <h1>Hello world</h1>
+
+
+</body>
+</html>
